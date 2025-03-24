7232a582ddca1887373274b53d25c8e88a9f0b2d
Nicokox1212 <144319801+Nicokox1212@users.noreply.github.com>
2025-03-24 17:56:50 +0100

    #7

---
+++ Nicola_allocca/wazenie.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styl.css">
+    <title>Ważenie samochodów ciężarowych</title>
+</head>
+<body>
+    <header>
+        <section class="baner1">
+        <h1>Ważenie pojazdów we Wrocławiu</h1>
+        </section>
+        <section class="baner2">
+        <img src="obraz1.png" alt="waga">
+        </section>
+    </header>
+    <section class="xd">
+    <section class="lewy">
+    <h2>Lokalizacje wag</h2>
+
+    <ol>
+        
+    </ol>
+
+    <h2>Kontakt</h2>
+    
+    <a href="mailto:nicowazenie@wroclaw.pl">napisz</a>
+
+    </section>
+    <section class="srodkowy">
+    <h2>Alerty</h2>
+
+    <table>
+        <tr>
+            <th>rejestracja</th>
+            <th>ulica</th>
+            <th>waga</th>
+            <th>dzien</th>
+            <th>czas</th>
+        </tr>
+
+    </table>
+
+    </section>
+    <section class="prawy">
+    <img src="obraz2.jpg" alt="tir">
+    </section>
+    </section>
+
+    <footer>
+        <p>Strone wykonal Nicola Allocca</p>
+    </footer>
+
+</body>
+</html>
